69efed6078c3255681e1ad5482f26d79a940b7cf
Priyanshu Singh <priyanshusinghdev2003@gmail.com>
2026-02-22 10:55:08 +0530

    Implement initial e-commerce home screen UI with core components and project configuration.

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,10 +1,161 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
+import { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Header from "@/components/Header";
+import { BANNERS, dummyProducts } from "@/assets/assets";
+import { useRouter } from "expo-router";
+import { CATEGORIES } from "@/constants";
+import CategoryItem from "@/components/CategoryItem";
+import { Product } from "@/constants/types";
+import ProductCard from "@/components/ProductCard";
+
+const { width } = Dimensions.get("window");
 
 export default function Home() {
+  const router = useRouter();
+  const [activeBannerIndex, setActiveBannerIndex] = useState<number>(0);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const categories = [{ id: "all", name: "All", icon: "grid" }, ...CATEGORIES];
+
+  const fetchProducts = async () => {
+    setProducts(dummyProducts);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className="flex-1" edges={["top"]}>
+      <Header showMenu showLogo showCart title="Forever" />
+      <ScrollView className="flex-1 px-4" showsVerticalScrollIndicator={false}>
+        {/* Banner Slider */}
+        <View className="mb-6">
+          <ScrollView
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            className="w-full h-48 rounded-xl"
+            scrollEventThrottle={16}
+            onScroll={(e) => {
+              const slide = Math.ceil(
+                e.nativeEvent.contentOffset.x /
+                  e.nativeEvent.layoutMeasurement.width,
+              );
+              if (slide !== activeBannerIndex) {
+                setActiveBannerIndex(slide);
+              }
+            }}
+          >
+            {BANNERS.map((banner, index) => (
+              <View
+                key={index}
+                className="relative w-full h-48 bg-gray-200 overflow-hidden"
+                style={{
+                  width: width - 32,
+                }}
+              >
+                <Image
+                  source={{ uri: banner.image }}
+                  className="w-full h-full"
+                  resizeMode="cover"
+                />
+                <View className="absolute bottom-4 left-4 z-10">
+                  <Text className="text-white text-2xl font-bold">
+                    {banner.title}
+                  </Text>
+                  <Text className="text-white text-sm font-medium">
+                    {banner.subtitle}
+                  </Text>
+                  <TouchableOpacity className="mt-2 bg-white px-4 py-2 rounded-full self-start">
+                    <Text className="text-primary font-bold text-xs">
+                      Get Now
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+                <View className="absolute inset-0 bg-black/40" />
+              </View>
+            ))}
+          </ScrollView>
+          {/* pagination dots */}
+          <View className="flex-row justify-center mt-3 gap-2">
+            {BANNERS.map((_, index) => (
+              <View
+                key={index}
+                className={`h-2 rounded-full ${index === activeBannerIndex ? "w-6 bg-primary" : "w-2 bg-gray-300"} transition-all duration-300`}
+              />
+            ))}
+          </View>
+        </View>
+        {/* categories */}
+        <View className="mb-6">
+          <View className="flex-row justify-between items-center mb-4">
+            <Text className="text-xl font-bold text-primary">Categories</Text>
+          </View>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {categories.map((cat: any) => (
+              <CategoryItem
+                key={cat.id}
+                item={cat}
+                isSelected={false}
+                onPress={() =>
+                  router.push({
+                    pathname: "/shop",
+                    params: { category: cat.id === "all" ? "" : cat.name },
+                  })
+                }
+              />
+            ))}
+          </ScrollView>
+        </View>
+
+        {/* popular products */}
+        <View className="mb-8 ">
+          <View className="flex-row justify-between items-center mb-4">
+            <Text className="text-xl font-bold text-primary">
+              Popular Products
+            </Text>
+            <TouchableOpacity onPress={() => router.push("/shop")}>
+              <Text className="text-seconday text-sm">See All</Text>
+            </TouchableOpacity>
+          </View>
+          {loading ? (
+            <ActivityIndicator size="large" />
+          ) : (
+            <View className="flex-row flex-wrap justify-between">
+              {products.slice(0, 4).map((product: Product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+            </View>
+          )}
+        </View>
+
+        {/* news letter CTA */}
+        <View className="bg-gray-100 p-6 rounded-2xl mb-20 items-center">
+          <Text className="text-2xl font-bold text-primary mb-2 text-center">
+            Join the Revolution
+          </Text>
+          <Text className="text-center mb-4 text-secondary">
+            Subscribe to our newsletter for exclusive offers and first look at
+            new products.
+          </Text>
+          <TouchableOpacity className="bg-primary w-4/5 py-3 items-center rounded-full">
+            <Text className="text-white font-medium text-base">
+              Subscribe Now
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
---
+++ client/components/ProductCard.tsx
@@ -0,0 +1,62 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { ProductCardProps } from "@/constants/types";
+import { Link } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "@/constants";
+
+export default function ProductCard({ product }: ProductCardProps) {
+  const isLiked = false;
+  return (
+    <Link href={`/product/${product._id}`} asChild>
+      <TouchableOpacity className="w-[48%] mb-4 bg-white rounded-lg overflow-hidden">
+        <View className="relative h-56 w-full bg-gray-100">
+          <Image
+            source={{ uri: product.images[0] }}
+            className="w-full h-full"
+            resizeMode="cover"
+          />
+          {/* heart icon */}
+          <TouchableOpacity
+            className="absolute top-2 right-2 z-10 bg-white rounded-full p-2 shadow-sm"
+            onPress={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <Ionicons
+              name={isLiked ? "heart" : "heart-outline"}
+              size={20}
+              color={isLiked ? COLORS.accent : COLORS.primary}
+            />
+          </TouchableOpacity>
+          {/* is Featured */}
+          {product.isFeatured && (
+            <View className="absolute top-2 left-2  bg-black rounded px-2 py-1">
+              <Text className="text-white text-xs font-bold uppercase">
+                Featured
+              </Text>
+            </View>
+          )}
+        </View>
+        {/* product info */}
+        <View className="p-3">
+          <View className="flex-row items-center  mb-1">
+            <Ionicons name="star" size={14} color="#FFD700" />
+            <Text className="text-secondary text-xs ml-1">4.6</Text>
+          </View>
+          <Text
+            className="text-primary font-medium text-sm mb-1"
+            numberOfLines={1}
+          >
+            {product.name}
+          </Text>
+          <View className="flex-row items-center">
+            <Text className="text-primary font-bold text-base">
+              ${product.price.toFixed(2)}
+            </Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </Link>
+  );
+}
